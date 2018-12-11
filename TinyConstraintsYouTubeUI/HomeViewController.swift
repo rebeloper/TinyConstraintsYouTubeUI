bfa0bdff6575de3b331772be59c1f32e8b4dbedd
Alex <therebeloper@gmail.com>
2018-12-11 10:26:21 +0200

    Initial Commit

---
+++ TinyConstraintsYouTubeUI/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  TinyConstraintsYouTubeUI
+//
+//  Created by Alex Nagy on 11/12/2018.
+//  Copyright © 2018 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
